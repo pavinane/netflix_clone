2f2605b958ad3ff735137ddf56e6d16ad11fbdf7
pavinane <pavimega77@gmail.com>
2023-03-29 10:51:26 +0530

    create netflix clone using nextjs, prisma and mongodb

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
-
 ## Getting Started
 
 First, run the development server:
@@ -14,25 +12,63 @@ pnpm dev
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+# Clone Netflix 
 
-## Learn More
+Create Netflix clone using Nextjs and Tailwind css and Using Backend for Prisma and MongoDb
 
-To learn more about Next.js, take a look at the following resources:
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+# Prisma
+Steps follow to install 
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+    --> npm  i prisma 
+
+
+    --> npm i init
+
+
+    --> npm install @prisma/client
+
+# Lib
+
+ create Lib folder 
+
+    --> create prismadb.ts file
+
+# Global
+
+
+    --> create global.d.ts file 
+
+# Mongodb
+
+    --> add database url in env file
+ 
+
+# Schema
+
+In schema file add model data query for netflix ,after using below command , this comman is using for push your db data query on your mongo db. 
+
+        -->     npx prisma db push
+
+By the time open your mongodb site 
+
+    -> database -> Browse collection -> collection
+
+ refresh  you will see the your database
+
+
+# API
+
+In api folder delete hellow.ts and create new file[...nextauth].ts and intall
+
+    --> npm install next-auth
+    --> npm install bcrypt
+    --> npm i -D @type/bcrypt
+ 
+    
+
 
 ## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
---
+++ pages/auth.tsx
@@ -0,0 +1,120 @@
+import Input from "@/components/input";
+import { useCallback, useState } from "react";
+import axios from 'axios'
+import {signIn} from 'next-auth/react';
+import { useRouter } from "next/router";
+
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter()
+
+  const [variant, setVariant] = useState("login");
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVarient) =>
+      currentVarient === "login" ? "register" : "login"
+    );
+  }, []);
+
+  const login = useCallback(async() => {
+    try {
+      await signIn('credentials',{
+        email,
+        password,
+        redirect:false,
+        callbackUrl:'/'
+      })
+      router.push('/')
+    } catch (error) {
+      console.log(error)
+    }
+
+  },[email, password,router])
+
+
+  const register = useCallback(async() => {
+    try {
+      await axios.post('/api/register',{
+        email,
+        name,
+        password
+      });
+      login()
+     
+    } catch (error) {
+      console.log(error)
+    }
+  },[email, name, password,login])
+
+ 
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" alt="logo" className="h-12" />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === "register" ? "Sign In" : " Register "}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant === "register" && (
+                <Input
+                  id="name"
+                  value={name}
+                  type="text"
+                  onChange={(e: any) => {
+                    setName(e.target.value);
+                  }}
+                  label="UserName"
+                />
+              )}
+
+              <Input
+                id="email"
+                value={email}
+                type="email"
+                onChange={(e: any) => {
+                  setEmail(e.target.value);
+                }}
+                label="Email"
+              />
+              <Input
+                id="password"
+                value={password}
+                type="password"
+                onChange={(e: any) => {
+                  setPassword(e.target.value);
+                }}
+                label="Password"
+              />
+              <button onClick={ variant === 'login' ? login : register} className=" rounded-md bg-red-600 hover:bg-red-700 text-white py-3 w-full transition">
+                {variant === "login" ? "Login" : "Sign up"}
+              </button>
+              <p className=" text-neutral-500 mt-12">
+                {
+                    variant === "login" ? "First Time using Netflix?":"Already have an account"
+                }
+                
+                <span
+                  onClick={toggleVariant}
+                  className="text-white ml-1 hover:underline cursor-pointer"
+                >
+                    {
+                        variant === "login" ? "Create an Acount":"Login"
+                    }
+                  
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
