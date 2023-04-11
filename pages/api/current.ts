87449dc5dc19b4bb83e932df2a05ad83459ea904
pavinane <pavimega77@gmail.com>
2023-04-11 08:10:51 +0530

    changes current api

--- pages/api/current.ts
+++ pages/api/current.ts
@@ -1,16 +1,17 @@
-import { NextRequest,NextResponse } from "next/server";
- import serverAuth from "@/lib/serverAuth";
+import { NextApiRequest, NextApiResponse } from "next";
+import serverAuth from "@/lib/serverAuth";
 
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    if (req.method !== 'GET') {
+      return res.status(405).end();
+    }
 
-export default async function handler(req:NextRequest,res:NextResponse) {
-    if(req.method !== "GET") {
-        return res.status(405).end();
-    }
-    try{
-        const {currentUser}:any = await serverAuth(req)
-        return res.status(200).json(currentUser)
-    }catch(error){
-        console.log(error)
-        return res.status(400).end()
-    }
+    const { currentUser } = await serverAuth(req,res);
+
+    return res.status(200).json(currentUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).end();
+  }
 }
